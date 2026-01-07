3f58a536622ec56e5c7c1235555e73507d7a5b97
TiFAu <turchin.anton2@gmail.com>
2026-01-08 00:17:04 +0200

    added additional conditions for adding a key and value to the resulting object

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,11 +6,11 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  const resultArr = sourceString.split(';');
-  const startValue = {};
+  const declarations = sourceString.split(';');
+  const initialStyles = {};
 
-  return resultArr.reduce((resultObject, currentString) => {
-    const [key, value] = currentString.split(':');
+  return declarations.reduce((resultObject, currentString) => {
+    const [key, value] = currentString.split(':', 2);
     const trimmedKey = key != null ? key.trim() : null;
     const trimmedValue = value != null ? value.trim() : null;
 
@@ -18,10 +18,17 @@ function convertToObject(sourceString) {
       return resultObject;
     }
 
-    resultObject[trimmedKey] = trimmedValue;
+    if (
+      (trimmedKey && trimmedValue !== undefined && trimmedValue !== null) ||
+      (trimmedValue !== undefined &&
+        trimmedValue !== null &&
+        trimmedValue !== '')
+    ) {
+      resultObject[trimmedKey] = trimmedValue;
+    }
 
     return resultObject;
-  }, startValue);
+  }, initialStyles);
 }
 
 module.exports = convertToObject;
